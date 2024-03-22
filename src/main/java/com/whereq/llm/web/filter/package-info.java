c8b4139d223394a847a7f49f4edc33e6a52d6ed3
WhereQ <whereq@gmail.com>
2024-03-22 15:01:26 -0400

    Clean up

--- src/main/java/com/whereq/llm/web/filter/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Request chain filters.
- */
-package com.whereq.llm.web.filter;
--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -1,22 +1,4 @@
-import 'react-toastify/dist/ReactToastify.css';
-import './app.scss';
-import 'app/config/dayjs';
-
-import React, { useEffect } from 'react';
-import { Card } from 'reactstrap';
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-
-import { useAppDispatch, useAppSelector } from 'app/config/store';
-import { getSession } from 'app/shared/reducers/authentication';
-import { getProfile } from 'app/shared/reducers/application-profile';
-import Header from 'app/shared/layout/header/header';
-import Footer from 'app/shared/layout/footer/footer';
-import ErrorBoundary from 'app/shared/error/error-boundary';
-import AppRoutes from 'app/routes';
-
-const baseHref = document.querySelector('base').getAttribute('href').replace(/\/$/, '');
-
+import React from 'react';
 export const App = () => {
   const paddingTop = '60px';
   return <div>WhereQ LLM Api</div>;
